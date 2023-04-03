d0852d8bddb41db6a5860811af2fbee0591f70c8
Bright <mensahokobright@gmail.com>
2023-04-03 16:29:45 +0000

    Done creating the first section

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body >
+
+    <section class="bg-slate-900 pb-28">
+  
+   <div class="flex justify-between ">
+   <div class="my-7">
+
+       <p class="mx-10 logo text-white">logo</p>
+   </div>
+   
+  <ul class="flex my-7">
+    <li class="mx-6 text-white">Catalog</li>
+    <li class="mx-6 text-white">Discuss</li>
+    <li class="mx-6 text-white">Get pro</li>
+    <li class="mx-6"><a class="bg-cyan-900 rounded-md px-3 text-white py-3 hover:bg-cyan-700" href="login.php">Log in </a></li>
+    <li class="mx-6"><a class="bg-rose-900 rounded-md px-3 text-white py-3 hover:bg-rose-700" href="login.php">Register </a></li>
+   
+  </ul>
+</div>
+
+<!-- heading -->
+
+<div class="text-center">
+    <h1 class=" text-white text-4xl font-bold mt-16">The best way to learn to code</h1>
+    <p class="text-white mx-48 mt-5 px-28">Courses designed by experts with real-world practice. Join our global community. It's free.</p>
+
+    <button class="bg-rose-900 text-white px-28 rounded-md text-center py-3 mt-8 hover:bg-rose-700 ">Start learning now!</button>
+
+    
+</div>
+
+<!-- in demand courses -->
+<p class="text-white font-bold mx-10 mt-28">Most in-demand coding courses</p>
+<div class="flex mt-5">
+
+    <!-- JAVASCRIPT -->
+    <div class="bg-white rounded-md mx-10 py-1 px-6 flex shadow-lg shadow-cyan-500/50 hover:bg-yellow-500 hover:text-white">
+        <img src="./js.webp" class="w-12 rounded-full object-contain mr-5" alt="image of javascript">
+        <h1 class="font-bold py-6">Introduction to JavaScript</h1>
+    </div>
+
+    <!-- JAVA -->
+    <div class="bg-white rounded-md mx-10 py-1 px-6 flex shadow-lg shadow-cyan-500/50 hover:bg-red-500 hover:text-white">
+        <img src="./java.webp" class="w-12 rounded-full object-contain mr-5" alt="image of javascript">
+        <h1 class="font-bold py-6">Introduction to Java</h1>
+    </div>
+
+    <!-- HTML -->
+    <div class="bg-white rounded-md mx-10 py-1 px-6 flex shadow-lg shadow-cyan-500/50 hover:bg-orange-600 hover:text-white">
+        <img src="./html image.png" class="w-12 rounded-full object-contain mr-5" alt="image of javascript">
+        <h1 class="font-bold py-6">Introduction to HTML</h1>
+    </div>
+
+    <!-- CSS -->
+
+    <div class="bg-white rounded-md mx-10 py-1 px-6 flex shadow-lg shadow-cyan-500/50 hover:bg-blue-600 hover:text-white">
+        <img src="./css.png" class="w-12 rounded-full object-contain mr-5" alt="image of javascript">
+        <h1 class="font-bold py-6">Introduction to css</h1>
+    </div>
+
+</div>
+
+</section>
+  
+    
+</body>
+</html>
