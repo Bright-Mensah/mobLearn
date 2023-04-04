fc05b6ae95f06ff4796caa9f9e6578d31528e3ca
Bright <mensahokobright@gmail.com>
2023-04-04 14:04:58 +0000

    adding changes to the landing page

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MOBLearn</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 </head>
 <body >
 
@@ -22,8 +23,8 @@
     <li class="mx-6 text-white">Catalog</li>
     <li class="mx-6 text-white">Discuss</li>
     <li class="mx-6 text-white">Get pro</li>
-    <li class="mx-6"><a class="bg-cyan-900 rounded-md px-3 text-white py-3 hover:bg-cyan-700" href="login.php">Log in </a></li>
-    <li class="mx-6"><a class="bg-rose-900 rounded-md px-3 text-white py-3 hover:bg-rose-700" href="login.php">Register </a></li>
+    <li class="mx-6"><a class="bg-cyan-900 rounded-md px-3 text-white py-3 hover:bg-cyan-700 cursor-pointer" id="btnLogin" >Log in </a></li>
+    <li class="mx-6"><a class="bg-rose-900 rounded-md px-3 text-white py-3 hover:bg-rose-700 cursor-pointer" id="btnSignUp" >Register </a></li>
    
   </ul>
 </div>
@@ -34,7 +35,7 @@
     <h1 class=" text-white text-4xl font-bold mt-16">The best way to learn to code</h1>
     <p class="text-white mx-48 mt-5 px-28">Courses designed by experts with real-world practice. Join our global community. It's free.</p>
 
-    <button class="bg-rose-900 text-white px-28 rounded-md text-center py-3 mt-8 hover:bg-rose-700 ">Start learning now!</button>
+    <button id="btnStartLearning" class="bg-rose-900 text-white px-28 rounded-md text-center py-3 mt-8 hover:bg-rose-700 ">Start learning now!</button>
 
     
 </div>
@@ -45,26 +46,26 @@
 
     <!-- JAVASCRIPT -->
     <div class="bg-white rounded-md mx-10 py-1 px-6 flex shadow-lg shadow-cyan-500/50 hover:bg-yellow-500 hover:text-white">
-        <img src="./js.webp" class="w-12 rounded-full object-contain mr-5" alt="image of javascript">
+        <img src="images/js.webp" class="w-12 rounded-full object-contain mr-5" alt="image of javascript">
         <h1 class="font-bold py-6">Introduction to JavaScript</h1>
     </div>
 
     <!-- JAVA -->
     <div class="bg-white rounded-md mx-10 py-1 px-6 flex shadow-lg shadow-cyan-500/50 hover:bg-red-500 hover:text-white">
-        <img src="./java.webp" class="w-12 rounded-full object-contain mr-5" alt="image of javascript">
+        <img src="images/java.webp" class="w-12 rounded-full object-contain mr-5" alt="image of javascript">
         <h1 class="font-bold py-6">Introduction to Java</h1>
     </div>
 
     <!-- HTML -->
     <div class="bg-white rounded-md mx-10 py-1 px-6 flex shadow-lg shadow-cyan-500/50 hover:bg-orange-600 hover:text-white">
-        <img src="./html image.png" class="w-12 rounded-full object-contain mr-5" alt="image of javascript">
+        <img src="images/html image.png" class="w-12 rounded-full object-contain mr-5" alt="image of javascript">
         <h1 class="font-bold py-6">Introduction to HTML</h1>
     </div>
 
     <!-- CSS -->
 
     <div class="bg-white rounded-md mx-10 py-1 px-6 flex shadow-lg shadow-cyan-500/50 hover:bg-blue-600 hover:text-white">
-        <img src="./css.png" class="w-12 rounded-full object-contain mr-5" alt="image of javascript">
+        <img src="images/css.png" class="w-12 rounded-full object-contain mr-5" alt="image of javascript">
         <h1 class="font-bold py-6">Introduction to css</h1>
     </div>
 
@@ -82,19 +83,19 @@
 
 
 <div class="bg-white rounded-md mx-5 py-1 px-2 my-10 shadow-lg shadow-cyan-500/50 drop-shadow-2xl hover:shadow-black hover:bg-rose-900 hover:text-white group">
-        <img src="./home-why-solo-1.svg" class=" rounded-full object-contain " alt="image of javascript">
+        <img src="images/home-why-solo-1.svg" class=" rounded-full object-contain " alt="image of javascript">
         <h1 class="font-bold py-3 text-center text-4xl">Tailored to you</h1>
         <p class="text-center text-xl text-slate-500 font-serif hover:text-white group-hover:text-white">No matter your experience level, you'll be <br> writing real, functional code within minutes <br> of starting your first course.</p>
     </div>
 
 <div class="bg-white rounded-md mx-2 py-1 px-2 my-10 shadow-lg shadow-cyan-500/50 hover:bg-purple-900 hover:shadow-yellow-900 hover:text-white group">
-        <img src="./home-why-solo-2.svg" class=" rounded-full object-contain" alt="image of javascript">
+        <img src="images/home-why-solo-2.svg" class=" rounded-full object-contain" alt="image of javascript">
         <h1 class="font-bold py-3 text-center text-4xl">Bite-sized</h1>
         <p class="text-center text-xl text-slate-500 font-serif group-hover:text-white">Go step-by-step through our unique <br> courses. Assess what you’ve learned with <br> in-lesson quizzes, and gradually advance your <br> skills with practice.</p>
     </div>
 
 <div class="bg-white rounded-md mx-5 py-1 px-2 my-10 shadow-lg shadow-cyan-500/50 hover:shadow-teal-200 hover:bg-green-900 hover:text-white group">
-        <img src="./home-why-solo-3.svg" class=" rounded-full object-contain " alt="image of javascript">
+        <img src="images/home-why-solo-3.svg" class=" rounded-full object-contain " alt="image of javascript">
         <h1 class="font-bold py-3 text-center text-4xl">Get Proof</h1>
         <p class="text-center text-xl text-slate-500 font-serif group-hover:text-white">Earn a certificate to validate your newly <br> acquired skills. Post it on social for others <br> to see.</p>
     </div>
@@ -113,12 +114,12 @@
         <!-- details -->
         <div class="flex justify-evenly">
             <div>
-                <img src="./home-perfect-platform-1-rebranding.svg" alt="">
+                <img src="images/home-perfect-platform-1-rebranding.svg" alt="">
                 <h2 class="text-center font-bold text-4xl mb-5">Students</h2>
                 <p class="text-xl text-center text-slate-500 tracking-wide">Prepping for the big test or want to ace your  first <br> interview? Use mobLearn's real-world practice to <br> reinforce what you've learned and get you ready for <br> that big moment.</p>
             </div>
             <div>
-                <img src="./home-perfect-platform-2.svg" alt="">
+                <img src="images/home-perfect-platform-2.svg" alt="">
                 <h2 class="text-center font-bold text-4xl mb-5">Professionals</h2>
                 <p class="text-xl text-center text-slate-500  tracking-wide">You can learn something totally new to advance your <br> career. Or maybe you just want to knock off the rust.<br> Try Moblearn to get access to a variety of courses, <br> from machine learning to web development.</p>
             </div>
@@ -183,5 +184,6 @@
         </div>
 
     </section>
+    <script src="script.js"></script>
 </body>
 </html>
---
+++ script.js
@@ -0,0 +1,15 @@
+
+// start learning button
+$('#btnStartLearning').click(function(){
+    window.location.href="http://localhost/moblearn/login.php";
+})
+
+// login button
+$('#btnLogin').click(function(){
+    window.location.href="http://localhost/moblearn/login.php";
+})
+
+// sign up button
+$('#btnSignUp').click(function(){
+    window.location.href="http://localhost/moblearn/login.php";
+})
